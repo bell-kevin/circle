9f2a97e48ab6a4b445a0e6dd209d387a6ca68282
Kevin Bell <bellKevin@pm.me>
2022-06-01 21:02:08 +0000

    Update Circle.java

--- circle/src/circle/Circle.java
+++ circle/src/circle/Circle.java
@@ -1,22 +1,16 @@
 package circle;
 
-import java.util.Scanner;
-
 public class Circle {
 
     public static void main(String[] args) {
-        Scanner computerKeyboardInput = new Scanner(System.in);
         System.out.println("Ch 6 Example - Circle - by Kevin Bell\n");
         circleClass spot;
-        double radius=0;
         spot = new circleClass();
-
         System.out.println("Original circle");
-        spot.initialize(radius);
-        spot.printCircleData(radius);
-
+        spot.initialize();
+        spot.printCircleData();
         System.out.println("\nChanged circle");
         spot.setRadius(5);
-        spot.printCircleData(radius);
+        spot.printCircleData();
     }
 }
