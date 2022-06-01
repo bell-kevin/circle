f94d5f802600312bc855aa09aa650ee041026d0e
Kevin Bell <bellKevin@pm.me>
2022-06-01 20:26:03 +0000

    Update Circle.java

--- circle/src/circle/Circle.java
+++ circle/src/circle/Circle.java
@@ -7,16 +7,16 @@ public class Circle {
     public static void main(String[] args) {
         Scanner computerKeyboardInput = new Scanner(System.in);
         System.out.println("Ch 6 Example - Circle - by Kevin Bell\n");
-        Circle spot;
-        spot = new Circle();
+        circleClass spot;
+        double radius=0;
+        spot = new circleClass();
 
         System.out.println("Original circle");
-        spot.initialize();
-        spot.printCircleData();
+        spot.initialize(radius);
+        spot.printCircleData(radius);
 
         System.out.println("\nChanged circle");
         spot.setRadius(5);
-        spot.printCircleData();
+        spot.printCircleData(radius);
     }
-
 }
