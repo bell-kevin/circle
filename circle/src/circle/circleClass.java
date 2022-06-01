7475ea64e32c0c6a082e9166a9d14ff39ff186fc
Kevin Bell <bellKevin@pm.me>
2022-06-01 21:02:40 +0000

    Update circleClass.java

--- circle/src/circle/circleClass.java
+++ circle/src/circle/circleClass.java
@@ -1,27 +1,32 @@
 package circle;
+
 import java.util.Scanner;
+
 public class circleClass {
+
     public double radius, diameter, circumfrence, area;
     Scanner computerKeyboardInput = new Scanner(System.in);
-    
-    
+
+    //Constructor
+    public circleClass() {
+    }
+
     public void setRadius(double radius) {
         this.radius = radius;
     }
-    
-    
-     public void initialize(double radius) {
-         System.out.print("Please enter the radius: ");
-         radius = computerKeyboardInput.nextDouble();
-         this.radius = radius;
+
+    public void initialize() {
+        System.out.print("Please enter the radius: ");
+        radius = computerKeyboardInput.nextDouble();
     }
-     
-     public void printCircleData(double radius) {
-         diameter = radius *2;
-         System.out.println(diameter);
-         circumfrence = 2* Math.PI * radius;
-         System.out.println(circumfrence);
-         area = Math.PI * radius * radius;
-         System.out.println(area);
-    }  
+
+    public void printCircleData() {
+        diameter = radius * 2;
+        System.out.println("Radius: " + radius);
+        System.out.println("Diameter: " + diameter);
+        circumfrence = 2 * Math.PI * radius;
+        System.out.println("Circumfrence: " +circumfrence);
+        area = Math.PI * radius * radius;
+        System.out.println("area: " +area);
+    }
 }
